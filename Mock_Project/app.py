7c62ec8d912be8cf475af7d22787298c15f8890b
sejError <13egormor@gmail.com>
2025-11-01 22:43:28 +0100

    nothing changed

--- Mock_Project/app.py
+++ Mock_Project/app.py
@@ -15,6 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db=SQLAlchemy(app)
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
