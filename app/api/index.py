0de9332453ec75bce7d26f8e637554360ba00e35
sejError <13egormor@gmail.com>
2025-12-14 10:14:43 +0100

    Removed "builds" section

--- app/api/index.py
+++ app/api/index.py
@@ -1,9 +1,9 @@
 import sys
 import os
 
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, base_dir)
-os.chdir(base_dir)
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
 
 from app import app
 
